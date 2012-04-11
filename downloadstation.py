8a6e9e60b4c9a68ffa36bd006289da5850afc7bd
eric feigner <eric@weareinstrument.com>
2012-04-10 19:52:49 -0700

    updating license info

--- downloadstation.py
+++ downloadstation.py
@@ -4,7 +4,6 @@
 Forked from Version 1.7 (http://downloadstation.jroene.de/)
 
 Copyright (C) 2011 Matthias Radig
-Portions Copyright (C) 2012 Feigner
 
 This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 3 of the License, or (at your option) any later version.
 
